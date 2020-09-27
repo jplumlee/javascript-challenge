fabe7294a28dcb63a06680c152470fd75525bb00
Justan <jplumlee@outlook.com>
2020-09-26 23:51:40 -0700

    Updated app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
-var tableData = data;
+let tableData = data,
 
-// YOUR CODE HERE!
+button = d3.select("#filter-btn");
+// form = d3.select("#form");
+
+// Get a reference to the table body
+const tbody = d3.select("tbody");
+
+tableData.forEach((ufoSighting) => {
+let row = tbody.append("tr");
+Object.values(ufoSighting).forEach(value => {
+    let cell = row.append("td");
+    cell.text(value);
+});
+});
+
+// Complete the event handler function for the form
+// Create event handlers 
+
+button.on("click", function() {
+
+// remove any children from the list
+tbody.html("");
+
+// Prevent the page from refreshing
+d3.event.preventDefault();
+
+// Select the input element and get the raw HTML node
+// Get the value property of the input element
+let inputElement = d3.select("#datetime"), 
+    inputValue = inputElement.property("value");
+    // filteredData = tableData;
+    // if (inputElement) {
+        filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // }
+
+console.log(filteredData);
+
+filteredData.forEach((ufoSighting) => {
+let row = tbody.append("tr");
+Object.values(ufoSighting).forEach(value => {
+    let cell = row.append("td");
+    cell.text(value);
+});
+});
+
+
+// let filteredData = tableData;
+
+// d3.select(tableData).text(filteredData);
+
+});
