9efbf754e1efd7e2d641e6aa5411b2310684ecc8
Justan <jplumlee@outlook.com>
2020-10-02 20:31:02 -0700

    Updated app.js in level-1 file.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,8 @@
 // from data.js
-let tableData = data,
-
-button = d3.select("#filter-btn");
-// form = d3.select("#form");
+let tableData = data;
 
 // Get a reference to the table body
-const tbody = d3.select("tbody");
+var tbody = d3.select("tbody");
 
 tableData.forEach((ufoSighting) => {
 let row = tbody.append("tr");
@@ -17,37 +14,30 @@ Object.values(ufoSighting).forEach(value => {
 
 // Complete the event handler function for the form
 // Create event handlers 
-
+let button = d3.select("#filter-btn");
 button.on("click", function() {
 
-// remove any children from the list
-tbody.html("");
+    // remove any children from the list
+    tbody.html("");
 
-// Prevent the page from refreshing
-d3.event.preventDefault();
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
-// Select the input element and get the raw HTML node
-// Get the value property of the input element
-let inputElement = d3.select("#datetime"), 
-    inputValue = inputElement.property("value");
-    // filteredData = tableData;
-    // if (inputElement) {
-        filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-    // }
+    // Select the input element and get the raw HTML node
+    // Get the value property of the input element
+    let inputElement = d3.select("#datetime"),
+        inputValue = inputElement.property("value");
+    let filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
-console.log(filteredData);
 
-filteredData.forEach((ufoSighting) => {
-let row = tbody.append("tr");
-Object.values(ufoSighting).forEach(value => {
-    let cell = row.append("td");
-    cell.text(value);
+    console.log(filteredData);
+
+    filteredData.forEach((ufoSighting) => {
+    let row = tbody.append("tr");
+    Object.values(ufoSighting).forEach(value => {
+        let cell = row.append("td");
+        cell.text(value);
 });
 });
-
-
-// let filteredData = tableData;
-
-// d3.select(tableData).text(filteredData);
 
 });
